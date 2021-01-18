b40ad066cd399c04971dba5fd295d6ac6d14e847
Desiree Herschberger <citizendez@gmail.com>
2021-01-18 14:32:32 -0500

    set up flask

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 def index():
 
     # use render_template to serve up the index.html
-    return # your-code-here
+    return render_template('index.html')
 
 @app.route("/samples")
 def samples():
-
     # open the json file, located at static/data/samples.json
+    with open('static/data/samples.json') as f:
     # use json.load() to read in the file as json
+        data = json.load(f)
     # return that json through the Flask endpoini
-    
-    return # your-code-here
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
