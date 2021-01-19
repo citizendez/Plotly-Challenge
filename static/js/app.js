c634143ddb988e9e18e765d5782201b3d549b93c
Desiree Herschberger <citizendez@gmail.com>
2021-01-19 13:54:57 -0500

    complete gauge

--- static/js/app.js
+++ static/js/app.js
@@ -14,22 +14,17 @@ function init() {
       d3.select('#selDataset').append('option').attr('value', name).text(name);
     })
     buildChart();
+    buildGauge();
+    buildMetadata();
     // Use the first sample from the list to build the initial plots
   });
-}
-
-/*
-   Hints: Create additional functions to build the charts,
-          build the gauge chart, set up the metadata,
-          and handle the event listeners
-
-   Recommended function names:
-    buildGauge()
-*/
+};
 function optionChanged(){
   buildChart();
+  buildGauge();
+  buildMetadata();
 };
-//Function to build bar chart
+//Function to build charts
 function buildChart(){
   //find sample by id
   var subjectID = d3.select('#selDataset').node().value;
@@ -76,14 +71,13 @@ function buildChart(){
   var data = [trace1];
   
   var layout = {
-    title: 'Each Sample',
+    title: 'OTU Sample',
     showlegend: true,
     height: 600,
     width: 1200
   };
   
   Plotly.newPlot('bubble', data, layout);
-  buildMetadata();
 };
 //Build Metadata
 function buildMetadata(){
@@ -91,6 +85,7 @@ function buildMetadata(){
     var subjectID = d3.select('#selDataset').node().value;
     let targetSample = sampleData.metadata.find(subject => subject.id == subjectID);
     var sampleMetadata = d3.select('#sample-metadata');
+    sampleMetadata.html('');
     var table = sampleMetadata.append('table');
     table.attr('class', 'table table-striped')
     Object.entries(targetSample).forEach(([key, value]) => {
@@ -100,11 +95,51 @@ function buildMetadata(){
       var cell = row.append('td');
       cell.text(value);
     })
-  console.log(targetSample);
+  //console.log(targetSample);
 }
 //Gauge Chart
 function buildGauge(){
+  var subjectID = d3.select('#selDataset').node().value;
+  let targetSample = sampleData.metadata.find(subject => subject.id == subjectID);
+  var gauge = d3.select('#gauge');
+  var washFreq = targetSample.wfreq;
+  console.log(washFreq);
+  //gaauge
+  var trace1 =
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: washFreq,
+      title: { text: "Belly Button Washing Fequency" },
+      type: "indicator",
+      mode: "gauge+number+delta",
+      delta: { reference: 5 },
+      gauge: {
+        axis: { range: [null, 10] },
+        steps: [
+          { range: [0, 1], color: "rgb(245, 249, 250)" },
+          { range: [1, 2], color: "rgb(225, 233, 235)" },
+          { range: [2, 3], color: "rgb(202, 216, 219)" },
+          { range: [3, 4], color: "rgb(184, 207, 212)" },
+          { range: [4, 5], color: "rgb(174, 203, 209)" },
+          { range: [5, 6], color: "rgb(163, 203, 212)" },
+          { range: [6, 7], color: "rgb(154, 207, 219)" },
+          { range: [7, 8], color: "rgb(154, 214, 227)" },
+          { range: [8, 9], color: "rgb(146, 217, 232)" },
+          { range: [9, 10], color: "rgb(142, 227, 245)" }
+        ],
+        threshold: {
+          line: { color: "red", width: 4 },
+          thickness: 0.75,
+          value: 10
+        }
+      }
+    };
+    var data = [
+      trace1
+    ]
   
+  var layout = { width: 475, height: 350, margin: { t: 0, b: 0 } };
+  Plotly.newPlot('gauge', data, layout);
 }
 // Initialize the dashboard
 init();
